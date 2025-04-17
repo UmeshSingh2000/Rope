a8d366a849ae8300869a33d4b4001282535eb8e3
Umesh Singh <umeshsinghmehta4@gmail.com>
2025-04-17 23:41:25 +0530

    Added Shadcn in frontend with tailwind configuration

--- Frontend/src/Pages/UserLogin.jsx
+++ Frontend/src/Pages/UserLogin.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
 const UserLogin = () => {
   return (
     <div>
       <h1 className='text-4xl'>user login</h1>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
     </div>
   )
 }
