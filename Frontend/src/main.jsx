6ba3ee10781ac8adbc94f9b350ffc6b0a836fb7b
CyberDemon2001 <vaibhavtyagi254@gmail.com>
2025-04-19 16:43:53 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,6 +6,13 @@ import UserLogin from "./Pages/UserLogin";
 import UserSignup from "./Pages/UserSignup";
 import Home from "./Pages/Home";
 import { Toaster } from "react-hot-toast";
+// App.js or index.js
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+// Add all solid icons to the library
+library.add(fas);
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
